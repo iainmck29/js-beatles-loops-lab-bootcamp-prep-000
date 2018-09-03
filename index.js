5cfdcc5f2aa8562b38277c2448562c5348c4bf5b
Iain McKenzie <iainmck29@hotmail.com>
2018-09-03 12:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return array;
+}
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+
+let i = 0
+function johnLennonFacts(facts) {
+  while (i < 5) {
+    incrementVariable(i);
+    
+    facts[i] + "!!!";
+  }
+  return facts;  
+}
